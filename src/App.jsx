62eda81d185ae3aa88c3b755181a853241cc9bbe
Peerasak Wangsom <pwangsom@gmail.com>
2022-10-22 14:00:58 +0700

    Delete 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button } from '@mui/material';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + MUI 5</h1>
-      <Button color="secondary">Secondary</Button>
-      <Button variant="contained" color="success">
-        Success
-      </Button>
-      <Button variant="outlined" color="error">
-        Error
-      </Button>      
-      <div className="card">
-      <Button variant="contained" color="success" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-      </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   )
 }
