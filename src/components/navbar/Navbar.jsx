64fe8e7ae38627fdcbe11b37c278dd53cbc0c1f5
Peerasak Wangsom <pwangsom@gmail.com>
2022-10-24 07:12:34 +0700

    Commit Navbar.jsx 1st accept

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,75 @@
 import * as React from 'react';
+
+import Logo from '../../asset/img/extreme-iii-logo-white.png'
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
-const Navbar = () => {
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+
     return (
-        <Box>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AgricultureIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 300,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
                     >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         EXTREME-III
                     </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
-}
 
-export default Navbar;
+                    <AgricultureIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 1,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 300,
+                            color: 'inherit',
+                            fontSize: '1.1rem',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        EXTREME-III
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1 }}>
+                    </Box>
+                    
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Button color="inherit">Login</Button>
+                    </Box>
+                    
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
