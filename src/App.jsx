135555340c675d3a77bc7ea51d756f9d57772c51
Peerasak Wangsom <pwangsom@gmail.com>
2022-10-24 07:33:48 +0700

    Create HomeLoggedin and NavbarLoggedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import Home from './pages/home/Home'
+import HomeLoggedin from './pages/home-loggedin/HomeLoggedin';
 
 import Box from '@mui/material/Box';
 import { ThemeProvider } from "@mui/material";
@@ -11,7 +12,7 @@ function App() {
   return (
     <ThemeProvider theme={appTheme}>
       <Box sx={{ flexGrow: 1 }}>
-          <Home />
+          <HomeLoggedin />
       </Box>
     </ThemeProvider>
   )
---
+++ src/pages/home-loggedin/HomeLoggedin.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/NavbarLoggedin"
+
+import React from 'react'
+import Box from '@mui/material/Box';
+
+const HomeLoggedin = () => {
+  return (
+    <Box>
+        <Navbar />
+    </Box>
+  )
+}
+
+export default HomeLoggedin;
